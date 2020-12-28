8a209325524867cb9c48e77990d92df0c651a310
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-28 13:23:59 -0700

    Add project files.

---
+++ SixPartAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SixPartAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Mickey", "Minnie", "Goofy", "Donald Duck" };
+            Console.WriteLine("Describe something that all disney characters can do.");
+            string userTxt = Console.ReadLine();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                   
+            }
+
+            
+        }
+    }
+}
